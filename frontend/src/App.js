62508aeee1c5b4d6a8275a1687164a22466f9608
Andrei Klimiankou <AKlimiankou@gomel.iba.by>
2022-09-09 12:32:47 +0300

    Adding a Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,17 +4,21 @@ import AuthPage from './pages/Auth';
 import BookingsPage from './pages/Bookings';
 import EventsPage from './pages/Events';
 import './App.css';
+import MainNavigation from './components/navigation/MainNavigation';
 
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Navigate to='/auth' replace />} />
-        <Route path='/auth' element={<AuthPage />} />
-        <Route path='/events' element={<EventsPage />} />
-        <Route path='/bookings' element={<BookingsPage />} />
-      </Routes>
+      <MainNavigation />
+      <main className='main-content'>
+        <Routes>
+          <Route path='/' element={<Navigate to='/auth' replace />} />
+          <Route path='/auth' element={<AuthPage />} />
+          <Route path='/events' element={<EventsPage />} />
+          <Route path='/bookings' element={<BookingsPage />} />
+        </Routes>
+      </main>
     </BrowserRouter>
   );
 }
